7cc31fd9c231aa7bc46d7b57a83c223627324303
Adrian N <adriaanov000@gmail.com>
2025-03-31 22:49:05 -0400

    Update SuperHeroes.java
    Co-Authored-By: Adrian N <186498660+adrianno216@users.noreply.github.com>

--- client/generator-test/src/main/java/io/smallrye/graphql/client/generator/test/SuperHeroes.java
+++ client/generator-test/src/main/java/io/smallrye/graphql/client/generator/test/SuperHeroes.java
@@ -3,6 +3,9 @@ package io.smallrye.graphql.client.generator.test;
 import io.smallrye.graphql.client.generator.GraphQLQuery;
 import io.smallrye.graphql.client.generator.GraphQLSchema;
 
+
+
+
 @GraphQLSchema("resource:schema.graphql")
 @GraphQLQuery("{ teams { name } }")
 @GraphQLQuery("query heroesLocatedIn($location: String) { heroesIn(location: $location) { name realName superPowers } }")
